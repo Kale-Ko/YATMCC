b8b7365a9ba7c8e713ef5decdacffb4327241658
Kale-Ko <github@kaleko.ga>
2021-11-05 16:14:04 -0400

    Update

--- Assets/Scripts/Sky.cs
+++ Assets/Scripts/Sky.cs
@@ -15,11 +15,7 @@ public class Sky : MonoBehaviour
 
     void Update()
     {
-        transform.position = (PlayerController.Instance != null ? PlayerController.Instance.transform.position : new Vector3(0, 0, 0));
 
-        transform.GetChild(0).rotation = Quaternion.Euler(((time / 24000) * 360) - 90, 0, 0);
-        transform.GetChild(1).rotation = Quaternion.Euler(((time / 24000) * 360) + 90, 0, 0);
-        transform.GetComponent<MeshRenderer>().material.mainTextureOffset = new Vector2(time / 24000, 0);
     }
 
     public void Tick()
